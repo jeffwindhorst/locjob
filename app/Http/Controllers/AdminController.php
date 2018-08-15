4d9a06178274b58f55e2270e91263d4e0c4e8b88
Jeff Windhorst <jeffwindhorst@gmail.com>
2018-08-15 18:37:10 -0400

    Added Spatie ACL, added admin controller and admin/dashboard view

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the admin dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin/dashboard');
+    }
+}
