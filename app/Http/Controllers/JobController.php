01c9ed6eb9ec665193279a43718ee6e2182cec85
Jeff Windhorst <jeffwindhorst@gmail.com>
2018-08-25 12:08:37 -0400

    Fixed data set populating the map

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -12,36 +12,17 @@ class JobController extends Controller
 {
     public function search(Request $request) {
         
-        $skills[] = $request->input('skills');
+        $skills = $request->input('skills');
         $jobsByCity = DB::select(DB::raw("SELECT cities.name, cities.state, cities.growth_from_2000_to_2013, cities.latitude, cities.longitude, SUM(jobs.id) as job_total "
                 . "FROM `cities` "
                 . "JOIN jobs ON cities.id = jobs.city_id "
                 . "JOIN job_skills ON jobs.id = job_skills.job_id "
+                . "WHERE job_skills.skill = '" .$skills . "' "
                 . "GROUP BY jobs.id "));    
 
-        echo '<pre>';
-        print_r($jobsByCity);
-        echo '</pre>';
-        exit;
-        $cities = City::with('skill', function($q) use($jobId) {
-            $q->where('id', $jobId);
-        })->get();
+        $jcities = json_encode($jobsByCity);
        
         
-        foreach($jobs as $job)
-        {
-            echo '<Pre>';
-            $cities = $job->job->city->get();
-            print_r($job->job->city->count());
-//            print_r($cities);
-            foreach($cities as $city) {
-                echo $city->name . ', ' .$city->state . ', ' . $city->population . ', ' . $city->longitude . ', ' . $city->latitude . '<br>';
-                exit;
-            }
-            echo '</pre>';
-            exit;
-        }
-        
-        return view('search_results', ['skills' => $skills, 'jobs' => $jobs]);
+        return view('search_results', ['skills' => $skills, 'jcities' => $jobsByCity]);
     }
 }
--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -9,22 +9,34 @@
     <h1><i class='fa fa-key'></i> Search Results</h1>
 
     <ul class="skill-list">
-        @foreach($skills  as $skill)
-        <li>{{ $skill}}</li>
-        @endforeach
+        <li>{{ $skills }}</li>
     </ul>
 
-    <div id="map" style="width: 500px; height: 500px; display: block; border: 1px solid red;"></div>
+    <div id="map" style="width: 800px; height: 500px; display: block; border: 1px solid red;"></div>
     <script>
-function initMap() {
-    var uluru = {lat: -25.344, lng: 131.036};
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-    var marker = new google.maps.Marker({position: uluru, map: map});
-}
+        var cities = <?php echo json_encode($jcities); ?>;
+        function initMap() {
+            var locations = cities;
+            var map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 10,
+              center: new google.maps.LatLng(41.850033, -87.6500523),
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+
+            var infowindow = new google.maps.InfoWindow();
+
+            var marker, i;
+
+            for (i = 0; i < locations.length; i++) {
+                marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(locations[i].latitude, locations[i].longitude),
+                    map: map
+                });
+            }
+        }
 
     </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=<?php echo config('app.mapsApiKey'); ?>&callback=initMap"async defer></script>
+    <script src="https://maps.googleapis.com/maps/api/js?sensor=false&callback=initMap"async defer></script>
 
 </div>
 
